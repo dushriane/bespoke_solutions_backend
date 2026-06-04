417745d4a813bf7bdab8cec0ed9fc373f0add929
dushriane <arianedushime@gmail.com>
2026-06-04 14:17:48 +0200

    update(admin): added fields for tables in admin portal

--- customization/admin.py
+++ customization/admin.py
@@ -1,5 +1,29 @@
 from django.contrib import admin
-from customization.models import Customization, DesignerAssignment, DesignPlacement
-admin.site.register(Customization)
-admin.site.register(DesignPlacement)
-admin.site.register(DesignerAssignment)
+from customization.models import Customization, DesignPlacement, DesignerAssignment
+
+
+@admin.register(Customization)
+class CustomizationAdmin(admin.ModelAdmin):
+    list_display = ("id", "customer", "product", "variant", "design", "status", "created_at")
+    search_fields = ("customer__email", "product__product_name", "variant__sku", "design_name", "custom_text")
+    list_filter = ("status", "created_at", "product", "design")
+    ordering = ("-created_at",)
+    sortable_by = ("id", "customer", "product", "variant", "design", "status", "created_at")
+
+
+@admin.register(DesignPlacement)
+class DesignPlacementAdmin(admin.ModelAdmin):
+    list_display = ("id", "customization", "side_position", "layer_order", "created_at")
+    search_fields = ("customization__id", "side_position")
+    list_filter = ("side_position", "created_at")
+    ordering = ("-created_at",)
+    sortable_by = ("id", "customization", "side_position", "layer_order", "created_at")
+
+
+@admin.register(DesignerAssignment)
+class DesignerAssignmentAdmin(admin.ModelAdmin):
+    list_display = ("id", "customization", "order_item", "designer", "assigned_by", "status", "deadline", "created_at")
+    search_fields = ("customization__id", "designer__email", "assigned_by__email", "remarks")
+    list_filter = ("status", "assigned_at", "deadline", "created_at")
+    ordering = ("-created_at",)
+    sortable_by = ("id", "customization", "order_item", "designer", "assigned_by", "status", "deadline", "created_at")
--- orders/admin.py
+++ orders/admin.py
@@ -1,8 +1,47 @@
 from django.contrib import admin
-from orders.models import *
-admin.site.register(Order)
-admin.site.register(Shipment)
-admin.site.register(Payment)
-admin.site.register(OrderItem)
-admin.site.register(ShipmentStatusHistory)
-admin.site.register(ShipmentStatus)
+from orders.models import Order, OrderItem, Payment, Shipment, ShipmentStatusHistory
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "price", "created_at", "updated_at")
+    search_fields = ("name", "description")
+    list_filter = ("created_at", "updated_at")
+    ordering = ("-created_at",)
+    sortable_by = ("id", "name", "price", "created_at", "updated_at")
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ("id", "order", "product_name", "quantity", "price")
+    search_fields = ("product_name", "order__name")
+    list_filter = ("order",)
+    ordering = ("order",)
+    sortable_by = ("id", "order", "product_name", "quantity", "price")
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ("id", "order", "amount", "payment_method", "payment_date")
+    search_fields = ("order__name", "payment_method")
+    list_filter = ("payment_method", "payment_date")
+    ordering = ("-payment_date",)
+    sortable_by = ("id", "order", "amount", "payment_method", "payment_date")
+
+
+@admin.register(Shipment)
+class ShipmentAdmin(admin.ModelAdmin):
+    list_display = ("id", "order", "tracking_number", "carrier", "shipment_date")
+    search_fields = ("tracking_number", "carrier", "order__name")
+    list_filter = ("carrier", "shipment_date")
+    ordering = ("-shipment_date",)
+    sortable_by = ("id", "order", "tracking_number", "carrier", "shipment_date")
+
+
+@admin.register(ShipmentStatusHistory)
+class ShipmentStatusHistoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "shipment", "status", "updated_at")
+    search_fields = ("shipment__tracking_number", "status")
+    list_filter = ("status", "updated_at")
+    ordering = ("-updated_at",)
+    sortable_by = ("id", "shipment", "status", "updated_at")
